8bc71c9e021bbe8d3b5441fff2f9bd8e8564fddd
ngushcharin <ngushcharin@ozon.ru>
2021-11-24 17:47:46 +0300

    Добавление миграции

---
+++ src/CSharpCourse.EmployeesService.Migrations/04_RenameFieldDateToConferenceDate.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace CSharpCourse.EmployeesService.Migrations
+{
+    [Migration(4)]
+    public class RenameFieldDateToConferenceDate: Migration
+    {
+        public override void Up()
+        {
+            Rename.Column("date")
+                .OnTable(Constants.TableNames.Conferences)
+                .To("conference_date");
+        }
+
+        public override void Down()
+        {
+            Rename.Column("conference_date").OnTable(Constants.TableNames.Conferences).To("date");
+        }
+    }
+}
